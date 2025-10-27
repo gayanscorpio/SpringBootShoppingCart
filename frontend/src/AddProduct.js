ef138a773cad5e804891a052ecdcabc213b81bee
Gayan Chandana <gayanscorpio@gmail.com>
2025-10-27 09:08:05 +0530

    add comments

--- frontend/src/AddProduct.js
+++ frontend/src/AddProduct.js
@@ -1,3 +1,10 @@
+/**
+ * User fills form.
+On submit → GraphQL createProduct mutation to backend.
+Backend saves product → returns new product data.
+Redux updates instantly (so user sees new product in list).
+Form resets.
+ */
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client/react';
 import { CREATE_PRODUCT } from './graphql/productQueries';
@@ -5,18 +12,29 @@ import { useDispatch } from 'react-redux';
 import { addProducts } from './slices/ProductSlice';
 
 function AddProduct() {
+    // useState manages form input fields.
+    //➡ State for product form fields.
+    //Each input field updates a part of this object.
     const [form, setForm] = useState({ name: '', description: '', price: 0, sku: '' });
+
+    //call createProduct({ variables: { input: form } }), it sends the mutation to backend.
     const [createProduct] = useMutation(CREATE_PRODUCT);
     const dispatch = useDispatch();
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
+        e.preventDefault(); //Prevents default page reload.
+
+        //Executes GraphQL mutation to create product in backend.
         const { data } = await createProduct({ variables: { input: form } });
+
+        //Adds new product into Redux state (instant UI update).
         dispatch(addProducts(data.createProduct));
+        //Clears form inputs after submit.
         setForm({ name: '', description: '', price: 0, sku: '' });
     };
 
     return (
+        //When submitted → calls handleSubmit().
         <form onSubmit={handleSubmit}>
             <input placeholder="Name" value={form.name} onChange={e => setForm({ ...form, name: e.target.value })} />
             <input placeholder="Description" value={form.description} onChange={e => setForm({ ...form, description: e.target.value })} />
